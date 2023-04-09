0e8789620b5a57a0df7a9a52628b54224303057b
Nick Wherthey <nwherthey@unc.edu>
2023-04-09 19:13:06 -0400

    foo

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import { rpsls } from '../lib/rpsls.js';
-import minimist from 'minimist';
+import { rpsls } from '../lib/rpsls.js'
+import minimist from 'minimist'
 
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2))
 
 const help_message = `Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
@@ -14,28 +14,28 @@ Examples:
   node-rps        Return JSON with single player RPS result.
                   e.g. {"player":"rock"}
   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}`;
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`
 
 const rules = `Rules for Rock Paper Scissors:
 - Scissors CUTS Paper
 - Paper COVERS Rock
-- Rock CRUSHES Scissors`;
+- Rock CRUSHES Scissors`
 
 if (args.help_message || args.h) {
-    console.log(help_message);
-    process.exit(0);
+    console.log(help_message)
+    process.exit(0)
 }
 if (args.rules || args.r) {
-    console.log(rules);
-    process.exit(0);
+    console.log(rules)
+    process.exit(0)
 }
-let shot = args._[0];
+let shoot = args._[0]
 
 try {
-    console.log(JSON.stringify(rps_function(shot)));
+    console.log(JSON.stringify(rpsls(shoot)))
     
 } catch (error){
-    console.log(help_message);
-    console.log(rules);
-    process.exit(1);
+    console.log(help_message)
+    console.log(rules)
+    process.exit(1)
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -3,7 +3,7 @@
 import { rpsls } from "../lib/rpsls.js"
 import minimist from 'minimist'
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2))
 
 const help_message = `Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
@@ -13,7 +13,7 @@ Examples:
   node-rpsls        Return JSON with single player RPSLS result.
                     e.g. {"player":"rock"}
   node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`
 
 const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Scissors CUTS Paper
@@ -25,23 +25,23 @@ const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Lizard EATS Paper
 - Paper DISPROVES Spock
 - Spock VAPORIZES Rock
-- Rock CRUSHES Scissors`;
+- Rock CRUSHES Scissors`
 
 if (args.help_message || args.h) {
-    console.log(help_message);
-    process.exit(0);
+    console.log(help_message)
+    process.exit(0)
 }
 if (args.rules || args.r) {
-    console.log(rules);
-    process.exit(0);
+    console.log(rules)
+    process.exit(0)
 }      
-let shoot = args._[0];
+let shoot = args._[0]
 
 try {
-    console.log(JSON.stringify((shoot)));
+    console.log(JSON.stringify((rpsls(shoot))))
     
 } catch (error){
-    console.log(help_message);
-    console.log(rules);
-    process.exit(1);
+    console.log(help_message)
+    console.log(rules)
+    process.exit(1)
 }
